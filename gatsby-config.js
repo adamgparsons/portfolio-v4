9b8c9a17d8e80d062576212d9930a46ee00ebe6d
adamgparsons <adamgeorgeparsons@gmail.com>
2020-02-02 17:59:58 +0000

    added md directory

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,11 +6,31 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    "gatsby-plugin-catch-links",
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
         path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: "pages",
+      },
+    },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [], // just in case those previously mentioned remark plugins sound cool :)
+      },
+    },
+    {
+      resolve: `gatsby-remark-images`,
+      options: {
+        maxWidth: 620,
       },
     },
     `gatsby-transformer-sharp`,
---
+++ src/pages/articles/second-post/index.md
@@ -0,0 +1,9 @@
+---
+path: "/second-post"
+date: 2020-07-12T17:12:33.962Z
+title: "My Second Gatsby Post"
+order: 2
+cover: "./two.jpg"
+---
+
+Lorem ipsum dolor amet irony crucifix food truck quinoa flannel brooklyn seitan everyday carry cardigan tbh glossier sartorial pickled raw denim. Sriracha yuccie kombucha, iceland banjo adaptogen humblebrag. Mustache semiotics activated charcoal kinfolk cliche messenger bag neutra kombucha tilde. Bicycle rights wayfarers pitchfork, organic banjo photo booth tacos disrupt pinterest art party. Cred four dollar toast hammock, tousled cray brooklyn DIY ramps cronut tote bag. Microdosing meggings skateboard health goth, kombucha chambray hexagon mumblecore tilde irony iPhone fashion axe.
